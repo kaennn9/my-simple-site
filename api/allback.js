fa0f67c82ace65e00a11c022cebaebcd6f6c3db8
kaennn9 <kaennn9@gmail.com>
2025-07-04 22:12:43 +0300

    Update allback.js

--- api/allback.js
+++ api/allback.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 
   const params = new URLSearchParams();
   params.append('client_id', '1389685428332003529'); // استبدلها
-  params.append('client_secret', 'xdERXlPC-cqoZnH8PU8mODHYndYCbmzD'); // استبدلها
+  params.append('client_secret', 'Jw5QEsG4Mx3bTkdZn8ucJdPUoMMhnrZ-'); // استبدلها
   params.append('grant_type', 'authorization_code');
   params.append('code', code);
   params.append('redirect_uri', 'https://your-vercel-site.vercel.app/api/callback');
